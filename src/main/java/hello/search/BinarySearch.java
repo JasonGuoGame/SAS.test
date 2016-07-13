c5c35f1ada1eb3dc2e75804b32c5775cfda6246b
Jason (Yin) Guo <yin.guo@sas.com>
2016-07-13 19:53:12 +0800

    add algorithm for BinarySearch

---
+++ src/main/java/hello/search/BinarySearch.java
@@ -0,0 +1,30 @@
+package hello.search;
+
+/**
+ * Created by scnyig on 7/13/2016.
+ */
+public class BinarySearch {
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = -1, end = nums.length;
+        int mid;
+        while (start + 1 < end) {
+            // avoid overflow when (end + start)
+            mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (end == nums.length || nums[end] != target) {
+            return -1;
+        } else {
+            return end;
+        }
+    }
+}
