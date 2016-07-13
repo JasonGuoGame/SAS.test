621b88605f4618764b6dd8029cb494c274f0e6a3
Jason (Yin) Guo <yin.guo@sas.com>
2016-07-13 19:45:04 +0800

    add algrhtim for string

--- src/main/java/hello/string/FindStr.java
+++ src/main/java/hello/string/FindStr.java
@@ -4,7 +4,7 @@ package hello.string;
  * Created by scnyig on 7/11/2016.
  */
 public class FindStr {
-    
+
     public int strStr(String source, String target) {
         if (source == null || target == null) {
             return -1;
@@ -25,4 +25,46 @@ public class FindStr {
         // did not find the target
         return -1;
     }
+
+    public static boolean anagram(String s, String t) {
+        // write your code here
+        if (s==null || t==null || s.length()!=t.length()) return false;
+            int[] ss = new int[256];
+            int[] tt = new int[256];
+            for (int i=0; i<s.length(); i++) {
+                ss[s.charAt(i)]++;
+                tt[t.charAt(i)]++;
+            }
+            for (int j=0; j<256; j++) {
+                if (ss[j] != tt[j])
+                    return false;
+            }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String a = "abcd";
+        String b = "acbd";
+        boolean flag = anagram2(a,b);
+        System.out.println(flag);
+    }
+
+    public static boolean anagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            count[(int) s.charAt(i)]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            count[(int) t.charAt(i)]--;
+            if (count[(int) t.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
