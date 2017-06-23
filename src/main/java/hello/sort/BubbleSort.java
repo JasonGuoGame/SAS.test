951bdc102c92ddbbd53ec8e128cb9d13aa47c24f
Jason (Yin) Guo <yin.guo@sas.com>
2017-06-23 15:20:12 +0800

    fix bug for bubble sort

--- src/main/java/hello/sort/BubbleSort.java
+++ src/main/java/hello/sort/BubbleSort.java
@@ -17,4 +17,27 @@ public class BubbleSort {
 
         return data;
     }
+
+    //如果轮询一趟都没有交换位置，则排序完成。
+    public static int[] bubble_sort(int[] data) {
+        if(data == null || data.length ==0) {
+            return null;
+        }
+
+        boolean flag = false;
+        for (int outer = data.length -1; outer >= 0 && !flag; outer--) {
+            flag = true;
+            for (int inner = 0; inner < outer;inner++) {
+                if(data[inner] > data[inner+1]) {
+                    int temp = data[inner];
+                    data[inner] = data[inner+1];
+                    data[inner+1] =temp;
+
+                    flag = false;
+                }
+            }
+        }
+
+        return data;
+    }
 }
