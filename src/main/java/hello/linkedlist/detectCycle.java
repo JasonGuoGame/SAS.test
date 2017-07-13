f5b00e3c99daef898df99dc08bab48c95e8b533d
Jason (Yin) Guo <yin.guo@sas.com>
2017-07-13 15:28:28 +0800

    add detect cycle on link list

---
+++ src/main/java/hello/linkedlist/detectCycle.java
@@ -0,0 +1,39 @@
+package hello.linkedlist;
+
+/**
+ * Created by scnyig on 7/4/2017.
+ * https://www.zybuluo.com/867976167/note/52199
+ */
+public class detectCycle {
+    public ListNode detectCycle(ListNode node) {
+        ListNode slow = node;
+        ListNode fast = node;
+        boolean isCycle = false;
+
+        while (fast != null) {
+            if(fast.next == null || fast.next.next == null) {
+                return null;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) {
+                isCycle = true;
+                break;
+            }
+        }
+
+        slow = node;
+        if(isCycle) {
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+        } else {
+            return null;
+        }
+
+        return  fast;
+    }
+}
