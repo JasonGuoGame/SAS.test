38c318eb2dde94781c705d7a59030cbc9b552fd4
Jason (Yin) Guo <yin.guo@sas.com>
2017-07-21 18:10:12 +0800

    do some test on lintcode

---
+++ src/main/java/hello/tree/BinaryTreePaths.java
@@ -0,0 +1,44 @@
+package hello.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by scnyig on 7/21/2017.
+ */
+public class BinaryTreePaths {
+    private List<List<Integer>> lists = new ArrayList<List<Integer>>();
+    private List<Integer> path =  new ArrayList<Integer>();
+    private int sum;
+
+    public List<List<Integer>> binaryTreePathSum(BinaryTree.TreeNode root, int target) {
+        sum = 0;
+        helper(root, target);
+        return lists;
+    }
+
+    private void helper(BinaryTree.TreeNode root, int target) {
+        // Write your code here`
+        if (root == null) {
+            return;
+        }
+
+        sum += root.value;
+        path.add(root.value);
+
+        if (root.left == null && root.right == null && sum == target) {
+            // compare sum & target
+            // add 的是path的reference
+            List<Integer> l = new ArrayList<Integer>();
+            for (int i = 0; i < path.size(); i++) {
+                l.add(path.get(i));
+            }
+            lists.add(l);
+        }
+        helper(root.left, target);
+        helper(root.right, target);
+        sum -= path.get(path.size() - 1);
+        path.remove(path.size() - 1);
+
+    }
+}
