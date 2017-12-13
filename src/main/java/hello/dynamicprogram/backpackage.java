dee17e5bb337b497393be04034371fbe793f40aa
Jason (Yin) Guo <yin.guo@sas.com>
2017-12-13 15:42:12 +0800

    implement back package problem

--- src/main/java/hello/dynamicprogram/backpackage.java
+++ src/main/java/hello/dynamicprogram/backpackage.java
@@ -2,6 +2,23 @@ package hello.dynamicprogram;
 
 /**
  * Created by scnyig on 12/4/2017.
+ * https://www.jiuzhang.com/solution/backpack-ii/
  */
-public class backpackage {
+public class Backpackage {
+    public int solution(int m, int[] A, int[] V) {
+        int[][] dp = new int[A.length+1][m+1];
+        for (int i = 0; i <= A.length;i++) {
+            for (int j = 0; j <=m;j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }else if(A[i-1] > j) {
+                    dp[i][j] = dp[i-1][j];
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-A[i-1]] + V[i-1]);
+                }
+            }
+        }
+
+        return dp[A.length][m];
+    }
 }
